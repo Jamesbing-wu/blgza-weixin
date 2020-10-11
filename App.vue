a8c55d06bcdd020e93dede12594fc1838724179a
吴炳金 <53973336+Jamesbing-wu@users.noreply.github.com>
2020-10-12 00:18:11 +0800

    Add files via upload

---
+++ App.vue
@@ -0,0 +1,42 @@
+<script>
+export default {
+	data(){
+		return{
+			
+		}
+	},
+	globalData:{
+		userInfo:null,
+		code:null,
+		openid:null,
+		phone:null,
+		sessionKey:null,
+		signin:0
+	},
+	onLaunch: function() {
+			console.log('App Launch');	
+			
+			
+		},
+	
+	onShow: function() {
+		console.log('App Show');
+		
+		
+	},
+	onHide: function() {
+		console.log('App Hide');
+	},
+	
+};
+</script>
+
+<style>
+/* 解决头条小程序组件内引入字体不生效的问题 */
+/* #ifdef MP-TOUTIAO */
+@font-face {
+	font-family: uniicons;
+	src: url('/static/uni.ttf');
+}
+/* #endif */
+</style>
